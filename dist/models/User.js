20ff3cbd2b18f3ec50c688a657fb5491e22c44b6
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-08-09 22:54:21 +0530

    Second Commit

---
+++ dist/models/User.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import valiator from "validator";
+const schema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: [true, "Please enter ID"],
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter email"],
+        unique: [true, "Email already exists"],
+        validator: valiator._default.isEmail,
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+    },
+    role: {
+        type: String,
+        required: [true, "Please enter role"],
+        enum: ["admin", "user"],
+        default: "user",
+    },
+    dob: {
+        type: Date,
+        required: [true, "Please enter DOB"],
+    },
+    gender: {
+        type: String,
+        required: [true, "Please enter gender"],
+        enum: ["Male", "Female"],
+    },
+}, {
+    timestamps: true,
+});
+schema.virtual("age").get(function () {
+    const today = new Date();
+    const dob = this.dob;
+    let age = today.getFullYear() - dob.getFullYear();
+});
+const user = mongoose.model("user", schema);
