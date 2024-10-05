f56ba6b9a2781cbc17991c095b31d330ee5dee5f
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-10-05 23:08:18 +0530

    Admin Dashboard Backend

--- src/controllers/stats.ts
+++ src/controllers/stats.ts
@@ -1,11 +1,17 @@
+import { myCache } from "../app.js";
 import { tryCatch } from "../Middlewares/error.js";
 
 export const adminDashboardStats = tryCatch(async (req, res, next) => {
+  let stats;
 
+  if (myCache.has("admin-stats"))
+    stats = myCache.get(JSON.parse("admin-stats") as string);
 
+  return res.status(200).json({
+    success: true,
+    stats,
+  });
 });
-
-
 
 export const getPieCharts = tryCatch(async (req, res, next) => {});
 export const getBarCharts = tryCatch(async (req, res, next) => {});
--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -9,13 +9,13 @@ import { adminOnly } from "../Middlewares/auth.js";
 
 const app = express();
 
-//route --  /api/v1/admin/dashboard
-app.get("/dashbaord", adminOnly, adminDashboardStats);
-//route -- /api/v1/admin/pie
+//route --  /api/v1/dashboard/stats
+app.get("/stats", adminOnly, adminDashboardStats);
+//route -- /api/v1/dashboard/pie
 app.get("/pie", adminOnly, getPieCharts);
-//route --  /api/v1/admin/bar
+//route --  /api/v1/dashboard/bar
 app.get("/bar", adminOnly, getBarCharts);
-//route --  /api/v1/admin/line
+//route --  /api/v1/dashboard/line
 app.get("/line", adminOnly, getLineCharts);
 
 export default app;
